b6a22779f0d62471ddef4203bf0a8b286898dbf9
Diana <dianavv3232@gmail.com>
2025-06-29 21:15:33 -0700

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,12 @@
+name = "Diana Veliz"
+nickname = "D"
+age = 16
+has_student_used_python_before = True
+interests = "reading, writing, music, and drawing"
+hobbies = {
+    "reading": "psychological horror and thrillers, along with historical fiction",
+    "writing": "school essays",
+    "music": "The Beatles, Sonic Youth, Nastyona, and No Doubt",
+    "drawing": "small doodles and sketches to pass time",
+    "preferred_color": "brown"
+}
