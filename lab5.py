7d9d547e9c089503b92a8a41b9d2aceb0ee7d194
Diana <dianavv3232@gmail.com>
2025-07-07 12:53:47 -0700

    Add files via upload

---
+++ lab5.py
@@ -0,0 +1,31 @@
+# Lab 5 Diana Veliz
+
+def cat_greeting(word):
+    print(f'Cat says {word}')
+    print('Cat says purr')
+
+    print(cat_greeting("hiss"))
+
+    def generate_superhero_power():
+        name = "Milo"
+        power = "super strength"
+        print (f"{ name}'s power is {power}")
+
+        generate_superhero_power()
+        power = "flying"
+        return power
+    print (generate_superhero_power())
+
+def generate_superhero_power2(user_name, super_power):
+    message = user_name + " has the power of " + super_power + "!"
+    return message
+
+
+print (generate_superhero_power2("milo", "teleportation"))
+
+
+def cat_greeting_loops(greeting):
+    for i in range(5):
+        print(f'The cat says {greeting}')
+
+        cat_greeting_loops("meow")
