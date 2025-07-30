985b347e3bad68b934a23a37de7301be92599b83
Diana <dianavv3232@gmail.com>
2025-07-30 11:14:12 -0700

    Add files via upload

--- lab5.py
+++ lab5.py
@@ -4,28 +4,30 @@ def cat_greeting(word):
     print(f'Cat says {word}')
     print('Cat says purr')
 
-    print(cat_greeting("hiss"))
+print(cat_greeting("hiss"))
 
-    def generate_superhero_power():
-        name = "Milo"
-        power = "super strength"
-        print (f"{ name}'s power is {power}")
+def generate_superhero_power():
+    name = "Milo"
+    power = "super strength"
+    print(f"{name}'s power is {power}")
+    power = "flying"
+    return power
 
-        generate_superhero_power()
-        power = "flying"
-        return power
-    print (generate_superhero_power())
+print(generate_superhero_power())
 
 def generate_superhero_power2(user_name, super_power):
     message = user_name + " has the power of " + super_power + "!"
     return message
 
-
-print (generate_superhero_power2("milo", "teleportation"))
-
+print(generate_superhero_power2("milo", "teleportation"))
 
 def cat_greeting_loops(greeting):
     for i in range(5):
         print(f'The cat says {greeting}')
 
-        cat_greeting_loops("meow")
+cat_greeting_loops("meow")
+
+def generate_multiple_powers():
+    list = ["invisibility", "super strength", "telekenisis"]
+    for i in range(5):
+        print(f"milo has {range}")
